18f88c9880ae44590d8cec903153fd2eff13c47b
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-25 14:22:15 +1200

    Check presence of a single spelled word in the dictionary

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,17 +1,20 @@
 class Placement
   def initialize(grid, dictionary)
+    @dictionary = dictionary
     @tiles = []
   end
 
-  def place_tile(tile, x, y)
-    @tiles << [tile, Position.new(x, y)]
+  def place_tile(letter, x, y)
+    @tiles << PlacedTile.new(letter, Position.new(x, y))
   end
 
   def valid?
     return false if @tiles.empty?
+    return false if duplicate_positions?
     return false unless (all_on_same_row? || all_on_same_column?)
     return false unless sorted_positions.include?(CENTRE)
     return false if any_gaps?
+    return false unless valid_word?
     @tiles.size > 1
   end
 
@@ -25,29 +28,37 @@ class Placement
   end
   CENTRE = Position.new(7, 7)
 
+  PlacedTile = Struct.new(:letter, :position)
+
+  def valid_word?
+    word = tiles_sorted_by_position.map(&:letter).join('')
+    @dictionary.include?(word)
+  end
+
   def any_gaps?
     positions = sorted_positions
     (positions.map(&:x).uniq != (positions.first.x..positions.last.x).to_a) ||
       (positions.map(&:y).uniq != (positions.first.y..positions.last.y).to_a)
   end
 
+  def duplicate_positions?
+    positions = sorted_positions
+    positions.uniq != sorted_positions
+  end
+
   def all_on_same_row?
-    x_coords.uniq.size == 1
+    sorted_positions.map(&:x).uniq.size == 1
   end
 
   def all_on_same_column?
-    y_coords.uniq.size == 1
+    sorted_positions.map(&:y).uniq.size == 1
+  end
+
+  def tiles_sorted_by_position
+    @tiles.sort_by(&:position)
   end
 
   def sorted_positions
-    @tiles.map { |tile, pos| pos }.sort
-  end
-
-  def x_coords
-    @tiles.map { |tile, pos| pos.x }
-  end
-
-  def y_coords
-    @tiles.map { |tile, pos| pos.y }
+    tiles_sorted_by_position.map(&:position)
   end
 end
--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -9,49 +9,57 @@ RSpec.describe Placement do
     end
   end
 
-  context "with an empty board" do
+  context "on an empty board" do
     let(:grid) { Grid.new }
-    let(:dictionary) { Set.new(["it"]) }
+    let(:dictionary) { Set.new(["IT"]) }
     let(:placement) { Placement.new(grid, dictionary) }
 
     it "rejects a single tile on the center square" do
-      placement.place_tile(Tile.new("I"), 7, 7)
+      placement.place_tile("I", 7, 7)
       expect(placement).to_not be_valid
     end
 
     it "allows a two-letter word spanning the center square" do
-      placement.place_tile(Tile.new("I"), 7, 7)
-      placement.place_tile(Tile.new("T"), 8, 7)
+      placement.place_tile("I", 7, 7)
+      placement.place_tile("T", 8, 7)
       expect(placement).to be_valid
     end
 
     it "requires that the center square be covered" do
-      placement.place_tile(Tile.new("I"), 8, 7)
-      placement.place_tile(Tile.new("T"), 9, 7)
+      placement.place_tile("I", 8, 7)
+      placement.place_tile("T", 9, 7)
       expect(placement).to_not be_valid
     end
 
     it "allows vertical words" do
-      placement.place_tile(Tile.new("I"), 7, 7)
-      placement.place_tile(Tile.new("T"), 7, 8)
+      placement.place_tile("I", 7, 7)
+      placement.place_tile("T", 7, 8)
       expect(placement).to be_valid
     end
 
     it "rejects tiles on a diagonal" do
-      placement.place_tile(Tile.new("I"), 7, 7)
-      placement.place_tile(Tile.new("T"), 8, 8)
+      placement.place_tile("I", 7, 7)
+      placement.place_tile("T", 8, 8)
       expect(placement).to_not be_valid
     end
 
     it "rejects non-contiguous tiles" do
-      placement.place_tile(Tile.new("I"), 7, 7)
-      placement.place_tile(Tile.new("T"), 9, 7)
+      placement.place_tile("I", 7, 7)
+      placement.place_tile("T", 9, 7)
       expect(placement).to_not be_valid
     end
 
-    # prevent invalid words
+    it "rejects tiles placed on the same square" do
+      placement.place_tile("I", 7, 7)
+      placement.place_tile("T", 7, 7)
+      expect(placement).to_not be_valid
+    end
 
-    # prevent placing two tiles on the same square
+    it "rejects words not in the dictionary" do
+      placement.place_tile("Z", 7, 7)
+      placement.place_tile("Q", 8, 7)
+      expect(placement).to_not be_valid
+    end
   end
 
 end
